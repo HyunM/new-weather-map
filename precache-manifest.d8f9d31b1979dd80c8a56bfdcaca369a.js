d62a42be32d969cbb433e4e7e4b97d5d07df7460
HyunM <wisek0226@gmail.com>
2020-07-01 12:59:04 -0700

    Updates

---
+++ precache-manifest.d8f9d31b1979dd80c8a56bfdcaca369a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc4bac6fb24082e55cff7581bdfe1d5",
+    "url": "/HyunM/new-weather-map/index.html"
+  },
+  {
+    "revision": "933f189606d6e0996320",
+    "url": "/HyunM/new-weather-map/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9d225e9f4c4a89a429ef",
+    "url": "/HyunM/new-weather-map/static/js/2.d56ab903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HyunM/new-weather-map/static/js/2.d56ab903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933f189606d6e0996320",
+    "url": "/HyunM/new-weather-map/static/js/main.efbd57ec.chunk.js"
+  },
+  {
+    "revision": "0955281352cac5c252c9",
+    "url": "/HyunM/new-weather-map/static/js/runtime-main.b7190279.js"
+  }
+]);
